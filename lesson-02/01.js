5decbacff17c3a819fcf86e7c35a327f8798f11d
Stanislav Sydorenko <goneinterlude@gmail.com>
2025-11-03 17:07:44 +0200

    hw 2.1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,12 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
-
+let isAccess =
+  (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
+console.log(isAccess);
 // your code
