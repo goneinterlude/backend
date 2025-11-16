76a26b6cb9040cd251cd1d2b46fa3d38d9eee72f
Stanislav Sydorenko <goneinterlude@gmail.com>
2025-11-16 14:51:14 +0200

    hw 5.2

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -30,9 +30,14 @@ console.log(gallery)
 */
 
 const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-}
+  "Mona Lisa": "Leonardo da Vinci",
+  "Starry Night": "Vincent van Gogh",
+  "The Scream": "Edvard Munch",
+};
 
-function updateGallery() {}
+function updateGallery(obj, nameOfArt, artist) {
+  obj[nameOfArt] = artist;
+}
+updateGallery(gallery, "Mona Lisa", "Leonardo da Vinci, 1503-1506");
+updateGallery(gallery, "The Persistence of Memory", "Salvador Dali");
+console.log(gallery);
